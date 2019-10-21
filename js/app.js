cc780c66c39426372d3064de17c8e192113381b3
Dynamic Coders <42337291+dynCode@users.noreply.github.com>
2019-10-21 10:27:29 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@
 
     module.controller('AppController', function ($scope, $http, $window, $rootScope, $sce) {
 		var apiURL = "http://radiobop.co.za/app/app.php";
-        $scope.streamURL = $sce.trustAsResourceUrl('http://radioinbox.co.za:8000/radiobop');
+        $scope.streamURL = $sce.trustAsResourceUrl('http://live.urbanza.co.za/radio/8020/radiobop?1565862890');
         $scope.mapURL = $sce.trustAsResourceUrl('https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d898.4074446340522!2d28.23540712923897!3d-25.74975399897759!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1e9561d05a38b6d7%3A0xe3dc9a63563b4156!2sRadio+Bop+South+Africa!5e0!3m2!1sen!2sza!4v1542789917287');
 		$scope.djs = [];
         $scope.errorCode = '';
