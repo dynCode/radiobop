e443d643e8a68c739e91b237f2a08292ab048432
Dynamic Coders <42337291+dynCode@users.noreply.github.com>
2019-11-14 09:38:13 +0200

    Secure Update Test

--- js/app.js
+++ js/app.js
@@ -5,14 +5,14 @@
     var module = angular.module('app', ['onsen', 'ngSanitize']);
 
     module.controller('AppController', function ($scope, $http, $window, $rootScope, $sce) {
-		var apiURL = "http://radiobop.co.za/app/app.php";
-        $scope.streamURL = $sce.trustAsResourceUrl('https://capeant.antfarm.co.za:8000/ptafm');
+		var apiURL = "https://radiobop.co.za/app/app.php";
+        $scope.streamURL = $sce.trustAsResourceUrl('https://live.urbanza.co.za/radio/8020/radiobop?1565862890');
         $scope.mapURL = $sce.trustAsResourceUrl('https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d898.4074446340522!2d28.23540712923897!3d-25.74975399897759!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1e9561d05a38b6d7%3A0xe3dc9a63563b4156!2sRadio+Bop+South+Africa!5e0!3m2!1sen!2sza!4v1542789917287');
 		$scope.djs = [];
         $scope.errorCode = '';
         
         $scope.playAudio = function() {
-            var audioUrl = "https://capeant.antfarm.co.za:8000/ptafm";
+            var audioUrl = "https://live.urbanza.co.za/radio/8020/radiobop?1565862890";
 
             // Play the audio file at url
             var my_media = new Media(audioUrl,
@@ -63,7 +63,6 @@ document.addEventListener('deviceready', function () {
         cordova.plugins.backgroundMode.disableWebViewOptimizations(); 
     });
 }, false);
-/*
 function playAudio() {
 	var control = document.getElementById("radio-box");
     control.play();
@@ -98,4 +97,3 @@ function togglePlay() {
 		pause.remove("inactive");
 	}
 }
-*/
